32e7b881e4f58be91467b7b8eeafd5f1b9e2dbb9
Chanaka Dilshan <chanakad633@gmail.com>
2024-12-11 01:24:12 +0530

    updated advanced filtering

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FilterEvents from './Filtering-component/FilterEvents.jsx';
-import FilterUsers from './Filtering-component/FilterUsers.jsx';
+import FilterEvents from './features/AdvancedFiltering/FilterEvents.jsx';
+import FilterUsers from './features/AdvancedFiltering/FilterUsers.jsx';
 
 const App = () => {
   return (
       <Router>
           <Routes>
               <Route path="/filter-events/:userId" element={<FilterEvents />} />
-              <Route path="/filter-users/:eventId" element={<FilterUsers />} />
+              <Route path="/filter-volunteers/:eventId" element={<FilterUsers />} />
           </Routes>
       </Router>
   );
