b9cfc146e8d31f7a2803ad65dbe6fbdd9561992b
Chanaka Dilshan <chanakad633@gmail.com>
2024-12-12 00:41:32 +0530

    new

---
+++ Volunnect_Front/src/components/FilterEvents.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import "./FilterEvents.css";
+import { BASE_URL } from '../../services/api.js';
+
+const FilterEvents = () => {
+    const { userId } = useParams();
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        fetch(`${BASE_URL}/filter/events/${userId}`)
+            .then((response) => response.json())
+            .then((data) => setEvents(data))
+            .catch((error) => console.error("Error fetching events:", error));
+    }, [userId]);
+
+    return (
+        <div className="events-container">
+            {events.map((event) => (
+                <div key={event.id} className="event-card">
+                    
+                    <p><h3>{event.title}</h3></p>
+                    <p>Type: {event.type}</p>
+                    <p>Start Date: {event.startDate}</p>
+                    <p>End Date:{event.endDate}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FilterEvents;
---
+++ Volunnect_Front/src/components/FilterUsers.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import './FilterUsers.css';
+import { BASE_URL } from '../../services/api';
+
+const FilterUsers = () => {
+    const { eventId } = useParams();
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`${BASE_URL}/filter/volunteers/${eventId}`)
+            .then((response) => response.json())
+            .then((data) => setUsers(data))
+            .catch((error) => console.error("Error fetching users:", error));
+    }, [eventId]);
+
+    return (
+        <div className="users-container">
+            {users.map((user) => (
+                <div key={user.id} className="user-card">
+                    <h3>{user.name}</h3>
+                    <p>Rating: {user.ratings}</p>
+                    <p>Interests: {user.interests.join(", ")}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FilterUsers;
