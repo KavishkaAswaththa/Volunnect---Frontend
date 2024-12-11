16057e34d9d3bff3693d0119c6dffae14672293e
KavishkaAswaththa <157552673+KavishkaAswaththa@users.noreply.github.com>
2024-12-12 02:17:30 +0530

    updated

--- Volunnect_Front/src/App.jsx
+++ Volunnect_Front/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import './styles/App.scss';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignupForm";
+import ResetPasswordForm from "./components/ResetPasswordForm";
+import UpdatePasswordForm from "./components/UpdatePasswordForm";
+import Home from "./components/Home"; // Create a Home component
+
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/reset-password" element={<ResetPasswordForm />} />
+        <Route path="/update-password" element={<UpdatePasswordForm />} />
+      </Routes>
+    </Router>
   );
 }
 
--- Volunnect_Front/src/components/NavBar.jsx
+++ Volunnect_Front/src/components/NavBar.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import '../styles/Navbar.scss';
-import logo from '../assets/logo.png';
-import profile from '../assets/profile.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.scss";
+import logo from "../assets/logo.png";
+import profile from "../assets/profile.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">
-      <img src = {logo}  alt="Tree Logo" />
-        <span>VOLUNNECT</span>
-      </div>
+      <li><Link to="/">
+        <div className="logo">
+          <img src={logo} alt="Tree Logo" />
+          <span>VOLUNNECT</span>
+        </div>
+        </Link>
+      </li>
       <ul className="nav-links">
-        <li>HOME</li>
-        <li>CATEGORIES</li>
-        <li>SERVICES</li>
-        <li>RESOURCE</li>
-        <li>LOGIN</li>
+        <li><Link to="/">HOME</Link></li>
+        <li><Link to="/categories">CATEGORIES</Link></li>
+        <li><Link to="/services">SERVICES</Link></li>
+        <li><Link to="/resource">RESOURCE</Link></li>
+        <li><Link to="/login">LOGIN</Link></li>
       </ul>
       <div className="profile-icon">
         <img src={profile} alt="Profile" />
